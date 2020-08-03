b369a4bfc14ce97d9f29d86dc267b73820e7549d
Go <fhqmtkfkd@naver.com>
2020-08-04 06:16:45 +0900

    refactor: move classes and functions here to clean winguitester(currently main())
    class WindowFinder
    class ChildWindowFinder
    GetChildWindows()
    find_targets()
    were moved and used by order_maker.py

---
+++ window_finder.py
@@ -0,0 +1,71 @@
+import win32gui
+import pywintypes
+import collections
+
+
+class WindowFinder:
+    def __init__(self, windowname):
+        self.count = 0
+        try:
+            win32gui.EnumWindows(self.__EnumWindowsHandler, windowname)
+        except pywintypes.error as e:
+            pass
+    def __EnumWindowsHandler(self, hwnd, extra):
+        wintext = win32gui.GetWindowText(hwnd)
+        if extra in win32gui.GetWindowText(hwnd):
+            self.__hwnd = hwnd
+            return pywintypes.FALSE
+        if wintext.find(extra) != -1:
+            self.__hwnd = hwnd
+            return pywintypes.FALSE  # FALSE는 예외를 발생시킵니다.
+
+    def GetHwnd(self):
+        return self.__hwnd
+
+    __hwnd = 0
+
+class ChildWindowFinder:
+    def __init__(self, parentwnd):
+        try:
+            win32gui.EnumChildWindows(parentwnd, self.__EnumChildWindowsHandler, None)
+        except pywintypes.error as e:
+            if e[0] == 0:
+                pass
+
+    def __EnumChildWindowsHandler(self, hwnd, extra):
+
+        self.__childwnds.append(hwnd)
+
+    def GetChildrenList(self):
+        return self.__childwnds
+
+    __childwnds = []
+def GetChildWindows(windowname):
+
+    wf = WindowFinder(windowname)
+    phwnd = wf.GetHwnd()
+
+    childrenlist = ChildWindowFinder(phwnd).GetChildrenList()
+
+    return phwnd, childrenlist
+
+def find_targets(childwnds, cf):
+    cntl_dict = cf.get_control_dict()
+    counter_dict = collections.defaultdict(int)
+    for child in childwnds:
+        #ctrl_id = win32gui.GetDlgCtrlID(child)
+        wnd_clas = win32gui.GetClassName(child)
+        wnd_text = win32gui.GetWindowText(child)
+        #wnd_text = "".join(wnd_text.splitlines())
+        #parent = win32gui.GetParent(child)
+
+        counter_dict[wnd_clas] += 1
+        cur_count = counter_dict[wnd_clas]
+        if wnd_clas == "WindowsForms10.EDIT.app.0.13965fa_r9_ad1":
+            wnd_clas = "EDIT"
+        elif wnd_clas == "WindowsForms10.Window.b.app.0.13965fa_r9_ad1":
+            wnd_clas = "BUTTON"
+        if wnd_clas in cntl_dict:
+            if cur_count in cntl_dict[wnd_clas]:
+                name = cntl_dict[wnd_clas][cur_count]
+                cntl_dict[wnd_clas][name] = child
