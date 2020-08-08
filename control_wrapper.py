6b2f0dcc76bb20f6edac2bad39fa94dd03489431
Go <fhqmtkfkd@naver.com>
2020-08-09 04:04:15 +0900

    add process to be ensure that target window is at the top

--- control_wrapper.py
+++ control_wrapper.py
@@ -17,19 +17,30 @@ OpenProcess = ctypes.windll.kernel32.OpenProcess
 WriteProcessMemory = ctypes.windll.kernel32.WriteProcessMemory
 ReadProcessMemory = ctypes.windll.kernel32.ReadProcessMemory
 
+
+def makeParam (x, y):
+    return ((y << 16) | (x & 0xFFFF))
+
 class Edit(object):
     def __init__(self, hwnd):
         self.hwnd = hwnd
     def set_text(self, text):
         win32gui.SendMessage(self.hwnd, win32con.WM_SETTEXT, None, text)
-    def send_endter(self):
+    def send_enter(self):
+        win32gui.SendMessage(self.hwnd, win32con.WM_CHAR, 0x0D, 0)
+    def post_enter(self):
         win32gui.PostMessage(self.hwnd, win32con.WM_KEYDOWN, 0x0D, 0)
         win32gui.PostMessage(self.hwnd, win32con.WM_KEYUP, 0x0D, 0)
 class Button(object):
     def __init__(self, hwnd):
         self.hwnd = hwnd
     def click(self):
-        win32gui.SendMessage(self.hwnd, win32con.BM_CLICK,0,0)
+        x = 5
+        y = 5
+        pos = makeParam(x,y)
+        win32gui.SendMessage(self.hwnd, win32con.WM_LBUTTONDOWN, 1, pos)
+        win32gui.SendMessage(self.hwnd, win32con.WM_LBUTTONUP, 1, pos)
+        #win32gui.SendMessage(self.hwnd, win32con.BM_CLICK,0,0)
         #win32gui.SendMessage(self.hwnd, win32con.BM_CLICK, 0, 0)
 
 class RadioGroup(object):
@@ -39,18 +50,23 @@ class RadioGroup(object):
         n = int(n)
         x = 19 + 50 * n
         y = 11
-        pos = self.makeParam(x,y)
+        pos = makeParam(x,y)
         win32gui.SendMessage(self.hwnd, win32con.WM_LBUTTONDOWN, 1, pos)
         win32gui.SendMessage(self.hwnd, win32con.WM_LBUTTONUP, 1, pos)
 
-    def makeParam(self, x, y):
-        return ((y << 16) | (x & 0xFFFF))
+
 
 class CheckBox(object):
     def __init__(self, hwnd):
         self.hwnd = hwnd
     def check(self):
-        pass
+        x = 11
+        y = 11
+        pos = makeParam(x,y)
+        #win32gui.SendMessage(self.hwnd, win32con.BM_SETCHECK, 0, 0)
+        win32gui.SendMessage(self.hwnd, win32con.WM_LBUTTONDOWN, 1, pos)
+        win32gui.SendMessage(self.hwnd, win32con.WM_LBUTTONUP, 1, pos)
+
 class Control(object):
     def __init__(self, hwnd):
         self.hwnd = hwnd
